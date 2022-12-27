3c69dc4d85eef35ca638b8165f59f49e69af07e1
Perkles <luanwesley.wll@gmail.com>
2022-12-27 10:30:45 -0300

    Adiciona menu reponsivo

---
+++ static/script.js
@@ -0,0 +1,11 @@
+/*Menu lateral - mobile*/
+
+const menuMobile = document.querySelector(".menu-mobile");
+const body = document.querySelector("body");
+
+menuMobile.addEventListener("click", () =>{
+    menuMobile.classList.contains("bi-list")
+    ? menuMobile.classList.replace("bi-list", "bi-x")/* If ternario  */
+    : menuMobile.classList.replace("bi-x", "bi-list");
+    body.classList.toggle("menu-nav-active");
+});
